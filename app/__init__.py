f85e0c4c319c94a8e2947985aa39c23f311c0087
Muchogo <ericmuchogo@gmail.com>
2019-01-29 13:39:23 +0300

    [Finishes __init__.py]

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+from flask_restful import Api
+from instance import config
+# Local imports
+from .api.v1 import v1
+from .db_con import db_migrate
+from .errors import errors
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config.DevelopmentConfig)
+    app.register_blueprint(v1)
+    app.register_blueprint(errors)
+    return app
